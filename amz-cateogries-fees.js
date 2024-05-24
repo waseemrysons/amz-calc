6ebdee213502fc666118a5398717121e87755e2e
waseemrysons <149214644+waseemrysons@users.noreply.github.com>
2024-05-24 15:37:51 +0500

    file uploaded

---
+++ amz-cateogries-fees.js
@@ -0,0 +1,200 @@
+{
+    "feeCategories" :[
+      {
+        "category": "Amazon Device Accessories",
+        "referralFee": 45.9,
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Automotive and Powersports",
+        "referralFee": "15.3% for the portion of the total sales price up to £45.00, 9.18% for any portion of the total sales price greater than £45.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Baby Products",
+        "referralFee": "8.16% for products with a total sales price up to £10.00, 15.3% for products with a total sales price greater than £10.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Backpacks and Handbags",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Beauty, Health, and Personal Care",
+        "referralFee": "8.16% for products with a total sales price up to £10.00, 15.3% for products with a total sales price greater than £10.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Beer, Wine, and Spirits",
+        "referralFee": "10.2%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Books",
+        "referralFee": "15.3%, And a closing fee of £0.50 per item sold",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Business, Industrial, and Scientific Supplies",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Compact Appliances",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Clothing and Accessories",
+        "referralFee": "For Prime selection with a total price above £40: 15.3% for products with a total price greater than £15 for the portion of the total price up to £40, and 7.14% for any portion of the total price greater than £40. For any selection with a total price up to £15: 8.16%. For any selection with a total price greater than £15: 15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Commercial Electrical and Energy Supplies",
+        "referralFee": "12.24%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Computers",
+        "referralFee": "7.14%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Consumer Electronics",
+        "referralFee": "7.14%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Cycling Accessories",
+        "referralFee": "8.16%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Electronic Accessories",
+        "referralFee": "15.3% for the portion of the total sales price up to ₤100.00, 8.16% for any portion of the total sales price greater than ₤100.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Eyewear",
+        "referralFee": "15.30%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Footwear",
+        "referralFee": "15.30%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Full-Size Appliances",
+        "referralFee": "7.14%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Furniture",
+        "referralFee": "15.3% for the portion of the total sales price up to £175.00, 10.2% for any portion of the total sales price greater than £175.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Grocery and Gourmet",
+        "referralFee": "8.16% for products with a total sales price up to £10.00, 15.3% for products with a total sales price greater than £10.00",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Handmade",
+        "referralFee": "12.24%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Home and Kitchen",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Jewelry",
+        "referralFee": "20.4% for the portion of the total sales price up to £225.00, 5.1% for any portion of the total sales price greater than £225.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Lawn and Garden",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Luggage",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Mattresses",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Music, Video, and DVD",
+        "referralFee": "15.3%, And a closing fee of £0.50 per item sold",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Musical Instruments and AV Production",
+        "referralFee": "12.24%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Office Products",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Pet Products",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Software",
+        "referralFee": "15.3%, And a closing fee of £0.50 per item sold",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Sports and Outdoors",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Tyres",
+        "referralFee": "7.14%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Tools and Home Improvement",
+        "referralFee": "13.26%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Toys and Games",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Video Games - Games and Accessories",
+        "referralFee": "15.3%, And a closing fee of £0.50 per item sold",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Video Game Consoles",
+        "referralFee": "8.16%, And a closing fee of £0.50 per item sold",
+        "perItemMinimumReferralFee": "Not applicable"
+      },
+      {
+        "category": "Watches",
+        "referralFee": "15.3% for the portion of the total sales price up to £225.00, 5.1% for any portion of the total sales price greater than £225.00",
+        "perItemMinimumReferralFee": 0.25
+      },
+      {
+        "category": "Everything else",
+        "referralFee": "15.3%",
+        "perItemMinimumReferralFee": 0.25
+      }
+    ]
+  }
+  
